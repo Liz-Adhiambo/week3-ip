2c69e426d64fcc0c042c7ddc705fb6fcdbc6b073
Liz <adhiamboliz3@gmail.com>
2022-03-13 21:08:09 +0300

    added licence

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 var contactForm = $("#contact-form");
 
 $(function() {
-    // what we do titles add bold class and center
-    $('.w-title').addClass("fw-bold text-center");
+    // what we do titles center
+    $('.w-title').addClass("text-center");
 
     contactForm.on("submit", function(e) {
         e.preventDefault();
         let formValid = validateContactForm();
         if (formValid) {
-            const name = $("input.name").val();
+            let name = $("input.name").val();
 
             alert(`Thank you ${name}, your message has been received`);
             clearForm();
